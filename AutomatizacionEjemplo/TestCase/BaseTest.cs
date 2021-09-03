bacd0821126d309a78c02968331c4981f8ed933d
Gustavo Alejandro <gajorquera@indracompany.com>
2021-09-03 18:18:42 -0400

    SeleniumEjemplo
    Se sube la solución de selenium.

---
+++ AutomatizacionEjemplo/TestCase/BaseTest.cs
@@ -0,0 +1,54 @@
+﻿using AutomatizacionEjemplo.Handler;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizacionEjemplo.TestCase
+{
+    /*
+     * Clase base para todos los test
+     */
+    public abstract class BaseTest
+    {
+        //Selenium Driver
+        protected IWebDriver Driver;
+
+        //Url de la aplicacion bajo prueba
+        protected string Url = ConfigurationManager.AppSettings["Url"];
+
+        //SetUp: Anotacion de Nunit para ejecutar un metodo antes de cada test
+        //Metodo para iniciar el navegador Chrome y navegar a una URL
+        [SetUp]
+        public void BeforeBaseTest()
+        {
+            Driver = new FirefoxDriver();
+            Driver.Navigate().GoToUrl(Url);
+
+        }
+
+        //TearDown: Anotacion de Nunit para ejecutar un metodo despues de cada test
+        //Metodo para cerrar el navegador y hacer una captura de pantalla si el test falla
+        [TearDown]
+        public void AfterBaseTest()
+        {
+
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+                ScreenShotHandler.TakeScreenShot(Driver);
+
+
+            if (Driver != null)
+            {
+                Driver.Quit();
+            }
+        }
+    }
+
+
+}
---
+++ AutomatizacionEjemplo/TestCase/EmployeeTest.cs
@@ -0,0 +1,42 @@
+﻿using AutomatizacionEjemplo.PageObject;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizacionEjemplo.TestCase
+{
+    /*Clase que contiene los casos de prueba del formulario de Empleado*/
+    [TestFixture] //Anotacion de Nunit para marcar una clase que contenga casos de prueba
+    class EmployeeTest : BaseTest
+    {
+       
+        //Page Object para el formulario Empleado
+        private EmployeePage EmployeePage;
+
+        //SetUp: Anotacion de Nunit para ejecutar un metodo antes de cada test
+        //Metodo para loguearse en la aplicacion
+        [SetUp]
+        public void BeforeTest()
+        {
+
+            LoginPage loginPage = new LoginPage(Driver);
+            EmployeePage = loginPage.LoginAs("admin", "admin123");
+
+        }
+        //TestCase: Anotacion de Nunit para marcar a un metodo como un caso de prueba automatizado con parametros
+        //Metodo que implementa el caso de prueba de adicionar empleado. El resultado esperado es que se adicione correctamente
+        [TestCase("pepe","pepe@gmail.com","Zona 5","458632")]
+        [TestCase("juan", "juan@gmail.com", "Zona 10", "111224")]
+        public void AddEmployeeTest(string name, string email, string address, string phone )
+        {
+            EmployeePage.AddEmployee(name,email,address,phone);
+            Assert.IsTrue(EmployeePage.IsSuccessAlertPresent());
+        }
+
+    }
+}
